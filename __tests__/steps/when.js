90bf94ba61a476ab2a9c0bd906a30abb39883e4a
Robby Pelssers <robby.pelssers@external.engie.com>
2023-01-22 13:16:10 +0100

    feat: downgrade packages to have the same env behaviour

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,4 +1,4 @@
-// require("dotenv").config("../../functions/confirm-user-signup/.en");
+const AWS = require("aws-sdk");
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler =
@@ -27,6 +27,41 @@ const we_invoke_confirmUserSignup = async (username, name, email) => {
   await handler(event, context);
 };
 
+const a_user_signs_up = async (password, name, email) => {
+  const cognito = new AWS.CognitoIdentityServiceProvider();
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
+
+  const signUpResp = await cognito
+    .signUp({
+      ClientId: clientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [{ Name: "name", Value: name }],
+    })
+    .promise();
+
+  const username = signUpResp.UserSub;
+  console.log(`[${email}] - user has signed up [${username}]`);
+
+  await cognito
+    .adminConfirmSignUp({
+      UserPoolId: userPoolId,
+      Username: username,
+    })
+    .promise();
+
+  console.log(`[${email}] - confirmed sign up`);
+
+  return {
+    username,
+    name,
+    email,
+  };
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
+  a_user_signs_up,
 };
